440954aadae633e95b3042db3ba14c04c3e3d6f9
Veemon22 <tonyleor@gmail.com>
2025-10-03 22:50:51 -0400

    Skeleton of the site

--- main.py
+++ main.py
@@ -3,15 +3,27 @@ from flask import render_template
 
 app = Flask(__name__)
 
+# Home Page
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
+# Dining Page
 @app.route('/dining')
 def dining():
     return render_template('dining.html')
 
+# Calender Page
+@app.route('/calender')
+def calender():
+    return render_template('calender.html')
+
+# Quizes Page
+@app.route('/quizes')
+def quizes():
+    return render_template('quizes.html')
+
+# About Page
+@app.route('/about')
+def about():
+    return render_template('about.html')
