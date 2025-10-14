9974844e19c4b9b7877dd51b88ee3c7ecbc0b569
Veemon22 <tonyleor@gmail.com>
2025-10-14 18:05:30 -0400

    Signin styling v1 done

--- static/js/signin.js
+++ static/js/signin.js
@@ -1,10 +1,7 @@
-function showPopup(show) {
-    if (show) {
-        const popup = document.getElementById('error-popup');
-        popup.style.display = 'block';
-
-        setTimeout(() => {
-            popup.style.display = 'none';
-        }, 3000);
-    }
+function showPopup() {
+    const popup = document.getElementById("error-popup");
+    popup.classList.add("show");
+    setTimeout(() => {
+        popup.classList.remove("show");
+    }, 3000);
 }
